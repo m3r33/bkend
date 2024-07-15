1d2a4f7e5c52e0092cfe49a0ab1393224abd0c7c
m3r33 <bad0men@proton.me>
2024-07-16 00:05:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.type('html').send("Hello There!"));
