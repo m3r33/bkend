c443d777076e52052905517d8d14b806ee4d45c8
m3r33 <bad0men@proton.me>
2024-07-15 23:49:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json()); // Add this middleware to parse JSON bodies
+app.use(express.json());
 
 app.get("/", (req, res) => res.type('html').send("Hello There!"));
 
 app.post("/endpoint", async (req, res) => {
-    const body = req.body; // No need to parse, express.json() does it
+    const body = req.body;
     console.log({ body });
-    return res.json(body); // Send the body back as JSON response
+    return res.json(body);
 });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
